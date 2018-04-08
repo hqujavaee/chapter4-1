a7305367f28fe2620b6d292a15ba9e13088d6a18
27597581 <27597581@qq.com>
2018-04-08 11:43:20 +0800

    第一次提交

---
+++ src/main/java/cn/edu/hqu/cst/javaee/HuaMent/config/RootConfig.java
@@ -0,0 +1,10 @@
+package cn.edu.hqu.cst.javaee.HuaMent.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("cn.edu.hqu.cst.javaee.HuaMent.entity")
+public class RootConfig {
+
+}
